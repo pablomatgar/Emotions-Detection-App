1d096dc07e2e2fb1455eaa5b45d4f961bccc270e
pablomatgar <pablomatgar@gmail.com>
2022-06-29 13:25:49 +0900

    feat: initial commit

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+
+import path from 'path';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+
+const SRC_DIR = path.resolve(__dirname, './src');
+const BUILD_DIR = path.resolve(__dirname, './www');
+const argvs = require('yargs').argv;
+const HOST = process.env.MONACA_SERVER_HOST || argvs.host || '0.0.0.0';
+
+export default {
+  plugins: [
+    reactRefresh(),
+  ],
+  root: SRC_DIR,
+  base: '',
+  publicDir: false,
+  build: {
+    outDir: BUILD_DIR,
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': SRC_DIR,
+    },
+  },
+  server: {
+    host: HOST,
+    port: 8080,
+  }
+};
